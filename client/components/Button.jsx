2ed556b65f74841dcbafb2845abc143c1f51e9bc
Frances Hughes <franceslhughes@hotmail.com>
2022-11-03 16:38:15 +1300

    added grey button

--- client/components/Button.jsx
+++ client/components/Button.jsx
@@ -5,7 +5,7 @@ function Button({ children }) {
     <div className="flex space-x-2 justify-center">
       <button
         type="submit"
-        className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+        className="inline-block px-6 py-2.5 bg-pink-400 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-700 hover:shadow-lg focus:bg-gray-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out"
       >
         {children}
       </button>
